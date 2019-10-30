4ac873a58f9ca77adb2b2e08b771bb4e0c46fe9a
Kimfucious <kimfucious@abts.io>
2019-10-31 05:26:32 +0700

    swap out large/small for client-01/client-02

--- site/profile/manifests/pasture/app.pp
+++ site/profile/manifests/pasture/app.pp
@@ -1,12 +1,12 @@
 class profile::pasture::app {
-  if $facts['fqdn'] =~ 'large' {
+  if $facts['fqdn'] =~ 'client-01' {
     $default_character = 'elephant'
     $db                = 'postgres://pasture:m00m00@puppet-client-03.hof.prv/pasture'
-  } elsif $facts['fqdn'] =~ 'small' {
+  } elsif $facts['fqdn'] =~ 'client-02' {
     $default_character = 'cow'
     $db                = 'none'
   } else {
-    fail("The ${facts['fqdn']} node name must match 'large' or 'small'.")
+    fail("The ${facts['fqdn']} node name must match 'client-01' or 'client-02'.")
   }
   class { 'pasture':
     default_message   => 'Hello Puppet!',
